5a35309a4aa64af245f95a21cb3d149066755f28
Conrado Simoes Pereira Gameiro <conrado.gameiro@tereos.com>
2025-10-02 13:40:25 -0300

    argparser test5

--- .github/validation_scripts/validate.py
+++ .github/validation_scripts/validate.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('file_list')
 
 a = parser.parse_args().file_list
-
+a_list = a.split(' ')
 print('fcuk the world')
-print(a)
+for file in a_list:
+    print(f'Arquivo {file} modificado')
